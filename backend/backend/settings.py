86da626ed0717d1fe765844e2536b619ea59d6ef
RasyKy <85433980+RasyKy@users.noreply.github.com>
2025-05-29 13:10:18 +0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
-
+# DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
